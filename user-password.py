248ab3142c485ba8a0ea82491697a45cfe1133b0
Maximilian Grundke <maximiliangrundke@googlemail.com>
2023-10-05 13:40:48 +0200

    Fix authentication problems due to bad formatting

--- user-password.py
+++ user-password.py
@@ -1,7 +1,4 @@
 import os
 from pywikibot.login import BotPassword
 
-(
-    os.environ["WIKIDATA_BOT_USERNAME"],
-    BotPassword(os.environ["WIKIDATA_BOT_SUFFIX"], os.environ["WIKIDATA_BOT_PASSWORD"]),
-)
+(os.environ["WIKIDATA_BOT_USERNAME"], BotPassword(os.environ["WIKIDATA_BOT_SUFFIX"], os.environ["WIKIDATA_BOT_PASSWORD"]))
